f9ba4501cfff14765b041f2c4668218f04d06c0d
D5lment <d5lment@gmail.com>
2023-08-24 17:14:25 -0600

    Updates on Day01

--- lib/day01/calories_counter.ex
+++ lib/day01/calories_counter.ex
@@ -1,47 +1,49 @@
 defmodule Day01.CaloriesCounter do
-@moduledoc """
-After read and count all the calories by each Elf
-identify the Elf carrying the most Calories.
-"""
+  @moduledoc """
+  After read and count all the calories by each Elf
+  identify the Elf carrying the most Calories.
+  """
 
-    def read_list_of_calories do
-        test_file =
-        "/Users/miriam.reyes/workspace/elixir/aoc2022/lib/resources/day01/day_01.txt"
+  def read_list_of_calories do
+    test_file =
+      "/Users/miriam.reyes/workspace/elixir/aoc2022/lib/resources/day01/day_01.txt"
 
-        {:ok, contents} = File.read(test_file)
+    {:ok, contents} = File.read(test_file)
 
-        contents
-        |> String.split("\n\n", trim: true)
-        |> Enum.reduce([], fn x, acc ->
-            total =
-                x
-                |> String.split("\n", trim: true)
-                |> Enum.map(fn item -> String.to_integer(item) end)
-                |> Enum.sum()
-            acc ++ [total]
-            end)
-        |> Enum.max
-    end
+    contents
+    |> String.split("\n\n", trim: true)
+    |> Enum.reduce([], fn x, acc ->
+      total =
+        x
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn item -> String.to_integer(item) end)
+        |> Enum.sum()
 
-    def top_three_calories do
-        test_file =
-        "/Users/miriam.reyes/workspace/elixir/aoc2022/lib/resources/day01/day_01.txt"
+      acc ++ [total]
+    end)
+    |> Enum.max()
+  end
 
-        {:ok, contents} = File.read(test_file)
-        
-        contents
-        |> String.split("\n\n", trim: true)
-        |> Enum.reduce([], fn x, acc ->
-            total =
-                x
-                |> String.split("\n", trim: true)
-                |> Enum.map(fn item -> String.to_integer(item) end)
-                |> Enum.sum()
-            acc ++ [total]
-            end)
-        |> Enum.sort
-        |> Enum.reverse
-        |> Enum.take(3)
-        |> Enum.sum
-    end
+  def top_three_calories do
+    test_file =
+      "/Users/miriam.reyes/workspace/elixir/aoc2022/lib/resources/inputs/day01/day_01.txt"
+
+    {:ok, contents} = File.read(test_file)
+
+    contents
+    |> String.split("\n\n", trim: true)
+    |> Enum.reduce([], fn x, acc ->
+      total =
+        x
+        |> String.split("\n", trim: true)
+        |> Enum.map(fn item -> String.to_integer(item) end)
+        |> Enum.sum()
+
+      acc ++ [total]
+    end)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
 end
