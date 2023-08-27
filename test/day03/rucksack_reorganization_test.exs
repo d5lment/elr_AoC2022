512b50bf3b84c844df74c1fd824ce2e1b2ccf417
D5lment <d5lment@gmail.com>
2023-08-26 18:11:02 -0600

    Day03 solution part two

---
+++ test/day03/rucksack_reorganization_test.exs
@@ -0,0 +1,7 @@
+defmodule Day03.RucksackReorganizationTest do
+  @moduledoc """
+  Only doctest were require for Day03.RucksackReorganization :)
+  """
+  use ExUnit.Case, async: true
+  doctest Day03.RucksackReorganization
+end
